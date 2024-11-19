b2476ddf9640107dad5b86dd464df6e3ee2f59f1
unknown <joaoLaureano@alunos.utfpr.edu.br>
2024-11-18 21:04:22 -0300

    Initial commit for branch Noce

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -17,4 +18,61 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+=======
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { ActivityIndicator, StyleSheet } from 'react-native';
+
+import { useFonts } from 'expo-font';
+
+import Report from './src/screens/Report';
+import SurveyActions from './src/screens/SurveyActions';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'AveriaLibre-Regular': require('./src/assets/fonts/AveriaLibre-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator size="large" color="#0000ff" />;
+  }
+
+  return (
+  <NavigationContainer>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#2B1D62',
+        },
+        headerTintColor: '#573FBA',
+        headerTitleStyle: {
+          color: '#fff',
+          fontFamily: 'AveriaLibre-Regular',
+          fontWeight: 'bold',
+        },
+        headerBackTitleVisible: false,
+      }}
+    >
+      <Stack.Screen
+        name="SurveyActions"
+        component={SurveyActions}
+        options={{ title: 'Carnaval', headerTitleStyle: { fontFamily: 'AveriaLibre-Regular', color: '#fff',}}}
+      />
+      
+      <Stack.Screen
+        name="Report"
+        component={Report}
+        options={{ title: 'Relatório', headerTitleStyle: { fontFamily: 'AveriaLibre-Regular', color: '#fff',}}}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+
+}
+
+const styles = StyleSheet.create({
+>>>>>>> d15074a (Commit no código completo)
 });
---
+++ src/screens/SurveyActions.js
@@ -0,0 +1,95 @@
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
+import React from 'react'
+
+export default function SurveyActions(props) {
+  const handleClickModify = () => {
+    props.navigation.navigate('Report')
+  }
+
+  const handleClickCollectData = () => {
+    props.navigation.navigate('Report')
+  }
+
+  const handleClickReport = () => {
+    props.navigation.navigate('Report')
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <SafeAreaView>
+        <View style={styles.buttonRow}>
+          <TouchableOpacity style={styles.button} onPress={handleClickModify}>
+            <Image 
+              source={require('../assets/modify.png')} 
+              style={styles.icon} 
+              alt="Modificar"
+            />
+            <Text style={styles.text}>
+              Modificar
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={handleClickCollectData}>
+            <Image 
+              source={require('../assets/collectData.png')} 
+              style={styles.icon} 
+              alt="Coletar dados"
+            />
+            <Text style={styles.text}>
+              Coletar dados
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={handleClickReport}>
+            <Image 
+              source={require('../assets/report.png')} 
+              style={styles.icon} 
+              alt="Relatório"
+            />
+            <Text style={styles.text}>
+              Relatório
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#573FBA',
+    alignItems: 'center',
+    padding: 60,
+  },
+  headerText: {
+    fontFamily: 'AveriaLibre-Regular',
+    color: '#fff',
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+  },
+  button: {
+    backgroundColor: '#2B1F5C',
+    width: 150,
+    height: 150,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    width: 60,
+    height: 60,
+    marginBottom: 8,
+  },
+  text: {
+    fontFamily: 'AveriaLibre-Regular',
+    color: '#fff',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
