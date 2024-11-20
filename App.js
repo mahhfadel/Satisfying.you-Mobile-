894c94ae832d87c348aba79a68f0291c57ef749e
Maria Clara Fadel <mariaclarapereira@alunos.utfpr.edu.br>
2024-11-20 16:41:26 -0300

    telas felipe

--- App.js
+++ App.js
@@ -1,25 +1,82 @@
-
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { NavigationContainer, DarkTheme } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { ActivityIndicator, StyleSheet, TouchableOpacity } from 'react-native';
-
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ActivityIndicator,
+  ScrollView,
+  SafeAreaView,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import CustomInput from "./src/components/CustomInput";
 import { useFonts } from "expo-font";
 
+import NewUser from "./src/screens/NewUser";
 
-import Coleta from "./src/screens/Coleta";
-import NewSurvey from './src/screens/NewSurvey';
-import EditSurvey from './src/screens/EditSurvey';
-import SurveyActions from './src/screens/SurveyActions';
-import Report from './src/screens/Report';
-import RecoverPassword from './src/screens/RecoverPassword';
-import Drawer from './src/screens/Drawer';
+function HomeScreen({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [emailError, setEmailError] = useState("");
 
-import { Ionicons } from '@expo/vector-icons';
-import { SurveyProvider } from './src/context/SurveyContext';
+  const validateEmail = (email) => {
+    if (!email.includes("@")) {
+      setEmailError("E-mail e/ou senha inválidos.");
+    } else {
+      setEmailError("");
+    }
+  };
 
-const Stack = createStackNavigator();
+  return (
+    <ScrollView style={styles.container}>
+      <SafeAreaView>
+        <View style={styles.icon}>
+          <Text style={styles.logo}>Satisfying.you</Text>
+          <MaterialIcons
+            name="sentiment-satisfied-alt"
+            size={50}
+            color="#FFFFFF"
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <CustomInput
+            label="E-mail"
+            value={email}
+            onChangeText={(text) => {
+              setEmail(text);
+              validateEmail(text);
+            }}
+          />
+          <CustomInput
+            label="Senha"
+            value={senha}
+            onChangeText={setSenha}
+            secureTextEntry={true}
+          />
+          {emailError ? (
+            <Text style={styles.errorText}>{emailError}</Text>
+          ) : null}
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.text}>Entrar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.buttonBlue}
+            onPress={() => navigation.navigate("NewUser")}
+          >
+            <Text style={styles.textButton}>Criar minha conta</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonGray}>
+            <Text style={styles.textButton}>Esqueci minha senha</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
+  );
+}
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -30,70 +87,90 @@ export default function App() {
     return <ActivityIndicator size="large" color="#0000ff" />;
   }
 
-  return (
+  const Stack = createStackNavigator();
 
-  <SurveyProvider>
-    <NavigationContainer theme={DarkTheme}>
+  return (
+    <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
-          headerStyle: {
-            backgroundColor: '#2B1D62',
-          },
-          headerTintColor: '#573FBA',
-          headerTitleStyle: {
-            color: '#fff',
-            fontFamily: 'AveriaLibre-Regular',
-            fontWeight: 'bold',
-          },
-          headerBackTitleVisible: false,
-          animationEnabled: true,
-          cardStyle: { backgroundColor: '#372775' }
+          headerStyle: { backgroundColor: "#573FBA" },
+          headerTintColor: "#fff",
+          headerTitleStyle: { fontFamily: "AveriaLibre-Regular" },
         }}
       >
-
-        <Stack.Screen name="Home" component={Drawer} options={{ headerShown: false}}/>
-
-        
-        <Stack.Screen name="RecoverPassword" component={RecoverPassword} options={{title: 'Recuperação de senha', headerTitleStyle: {fontFamily: 'AveriaLibre-Regular', color: '#fff'}}}/>
         <Stack.Screen
-          name="NewSurvey"
-          component={NewSurvey}
-          options={{ title: 'Nova Pesquisa', headerTitleStyle: { fontFamily: 'AveriaLibre-Regular', color: '#fff',}}}
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
         />
         <Stack.Screen
-          name="EditSurvey"
-          component={EditSurvey}
-          options={{ title: 'Modificar Pesquisa', headerTitleStyle: { fontFamily: 'AveriaLibre-Regular', color: '#fff',}}}
+          name="NewUser"
+          component={NewUser}
+          options={{ title: "Novo Usuário" }}
         />
-
-        <Stack.Screen
-                name="SurveyActions"
-                component={SurveyActions}
-                options={{ title: 'Carnaval', headerTitleStyle: { fontFamily: 'AveriaLibre-Regular', color: '#fff',}}}
-      />
-      
-      <Stack.Screen
-        name="Report"
-        component={Report}
-        options={{ title: 'Relatório', headerTitleStyle: { fontFamily: 'AveriaLibre-Regular', color: '#fff',}}}
-      />
-
-        <Stack.Screen
-          name="Coleta"
-          component={Coleta}
-          options={{
-            title: "Coleta",
-            headerTitleStyle: {
-              fontFamily: "AveriaLibre-Regular",
-              color: "#fff",
-            },
-          }}
-        />
-
       </Stack.Navigator>
     </NavigationContainer>
-  </SurveyProvider>
-);
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#573FBA",
+    flex: 1,
+  },
+  logo: {
+    color: "#FFFFFF",
+    fontSize: 46,
+    fontFamily: "AveriaLibre-Regular",
+    marginRight: 25,
+  },
+  icon: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  inputContainer: {
+    width: "60%",
+    margin: "auto",
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#37BD6D",
+    padding: 8,
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  buttonBlue: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#419ED7",
+    padding: 6,
+    marginTop: 8,
+  },
+  buttonGray: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#B0CCDE",
+    padding: 6,
+    marginTop: 8,
+  },
+  text: {
+    fontFamily: "AveriaLibre-Regular",
+    color: "#fff",
+    fontSize: 16,
+  },
+  textButton: {
+    fontFamily: "AveriaLibre-Regular",
+    color: "#fff",
+    fontSize: 15,
+  },
+  errorText: {
+    marginTop: 4,
+    color: "#FD7979",
+    fontSize: 15,
+    fontFamily: "AveriaLibre-Regular",
+  },
+});
