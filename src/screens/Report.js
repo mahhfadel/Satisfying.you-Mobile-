3e43e826e0b28be5c28f2a579a7d157404625cba
João Gabriel Noce Laureano <joaolaureano@alunos.utfpr.edu.br>
2025-02-05 20:32:45 -0300

    Relatório corrigido para utilizar do firebase

--- src/screens/Report.js
+++ src/screens/Report.js
@@ -1,22 +1,71 @@
 import { SafeAreaView, StyleSheet, View, Text } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PieChart from 'react-native-pie-chart';
+import { doc, getDoc } from 'firebase/firestore';
+import { firebaseApp, db } from '../firebase/firebaseConfig';
 
-export default function Report() {
+export default function Report({ route }) {
   const widthAndHeight = 250;
-  const series = [15, 25, 30, 20, 10]; 
+  const [series, setSeries] = useState([]);
+  const [filteredColors, setFilteredColors] = useState([]);
+  const [filteredLabels, setFilteredLabels] = useState([]);
+
   const sliceColor = ['#008000', '#00FF00', '#FFD700', '#FF4500', '#FF0000'];
-  const labels = ['Excelente', 'Bom', 'Neutro', 'Ruim', 'Péssimo']; 
+  const labels = ['Excelente', 'Bom', 'Neutro', 'Ruim', 'Péssimo'];
+  const { id } = route.params || {};
+  const [pVotes, setPVotes] = useState(null);
+
+  useEffect(() => {
+    if (id) {
+      const surveyRef = doc(db, 'surveys', id);
+
+      getDoc(surveyRef)
+        .then((docSnapshot) => {
+          if (docSnapshot.exists()) {
+            const surveyData = docSnapshot.data();
+            setPVotes(surveyData.votes);
+          } else {
+            console.log('Documento não encontrado!');
+          }
+        })
+        .catch((error) => {
+          console.error('Erro ao pegar os dados: ', error);
+        });
+    }
+  }, [id]);
+
+  useEffect(() => {
+    if (pVotes) {
+      const votes = [
+        pVotes.excellent || 0,
+        pVotes.good || 0,
+        pVotes.neutral || 0,
+        pVotes.bad || 0,
+        pVotes.poor || 0,
+      ];
+
+      // Filtra dados, cores e rótulos com base em valores não zerados
+      const nonZeroData = votes
+        .map((value, index) => ({ value, color: sliceColor[index], label: labels[index] }))
+        .filter((item) => item.value > 0);
+
+      setSeries(nonZeroData.map((item) => item.value));
+      setFilteredColors(nonZeroData.map((item) => item.color));
+      setFilteredLabels(nonZeroData.map((item) => item.label));
+    }
+  }, [pVotes]);
 
   return (
     <View style={styles.container}>
       <SafeAreaView>
         <View style={styles.chartAndLegendContainer}>
-          <PieChart widthAndHeight={widthAndHeight} series={series} sliceColor={sliceColor} />
+          {series.length > 0 && (
+            <PieChart widthAndHeight={widthAndHeight} series={series} sliceColor={filteredColors} />
+          )}
           <View style={styles.legendContainer}>
-            {labels.map((label, index) => (
+            {filteredLabels.map((label, index) => (
               <View key={index} style={styles.legendItem}>
-                <View style={[styles.colorBox, { backgroundColor: sliceColor[index] }]} />
+                <View style={[styles.colorBox, { backgroundColor: filteredColors[index] }]} />
                 <Text style={styles.legendText}>{label}</Text>
               </View>
             ))}
@@ -36,8 +85,8 @@ const styles = StyleSheet.create({
     padding: 30,
   },
   chartAndLegendContainer: {
-    flexDirection: 'row', 
-    alignItems: 'center', 
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   legendContainer: {
     marginLeft: 20,
